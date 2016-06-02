6fc3b9a8f81ab454417286e8c87f778b8a88f178
Cam <git@psyko.ca>
2016-06-02 07:18:56 -0700

    Update waterMon.py
    Alphabetize function defs and imports
    Add close DB on exit
    Add valve close on exit

--- waterMon.py
+++ waterMon.py
@@ -4,16 +4,23 @@
 Monitor a reed switch for water flow (Gallon per pulse) add each debounced event to an sqlite database.
 Also have scheduling for a high flow valve with an <open> per <time> when enabled.
 """
+import atexit
 import datetime
-import time
 import sqlite3
+import time
 
-import schedule
 import pigpio
+import schedule
 
 
 def check_gpio_daemon():
 	return true
+
+
+def clean_exit_gpio():
+	thisPi.stop()
+	print("See you later!")
+
 
 def connect_db(db_filename):
 	"""Connect to DB file"""
@@ -21,20 +28,23 @@ def connect_db(db_filename):
 	c = conn.cursor()
 	return conn, c
 
+
+def db_export():
+	out = c.execute("SELECT * FROM events")
+	with open('latest.csv', 'w', newline='') as outfile:
+		for i in out:
+			a = out.fetchone()
+			csv.writer(outfile).writerow(a)
+
+
 def db_write():
 	c.execute("INSERT INTO events VALUES (NULL, CURRENT_TIMESTAMP)")
 	conn.commit()
-	return None
-
-		
-def pi_callback(gpio, level, tick):
-	global event
-	if event is None:
-		event = time.time()
-		print("Event!: ", time.strftime("%H:%M:%S",event))
 
 	
-	
+def exit_message():
+	print("Exited cleanly, see you later!")
+
 
 def off_list_gen(onList, onMinutes):
 	wl = []
@@ -58,34 +68,30 @@ def on_list_gen(startTime, endTime, outOfMinutes):
 		wt = wt + datetime.timedelta(minutes = outOfMinutes)
 	return wl
 
+
+def pi_callback(gpio, level, tick):
+	global event
+	if event is None:
+		event = time.time()
+		print("Event!: ", time.strftime("%H:%M:%S",event))
 	
-def db_export():
-	out = c.execute("SELECT * FROM events")
-	with open('./latest.csv', 'w', newline='') as file:
-		for i in out:
-			a = out.fetchone()
-			csv.writer(file).writerow(a)
-
-
 
 def setSchedule(onList, offList):
 	for i in range(len(onList)):
 		schedule.every().day.at(onList[i]).do(valve_on)
 		schedule.every().day.at(offList[i]).do(valve_off)
 
+
+def valve_off():
+	global GPIORELAYPIN1
+	thisPi.write(GPIORELAYPIN1, 0)
+
+
 def valve_on():
 	global GPIORELAYPIN1
 	thisPi.write(GPIORELAYPIN1, 1)
 	
 
-
-def valve_off():
-	global GPIORELAYPIN1
-	thisPi.write(GPIORELAYPIN1, 0)
-	
-	
-	
-	
 
 #set constants
 GPIORELAYPIN1 = 4
@@ -124,6 +130,11 @@ if __name__ == '__main__':
 	onList = on_list_gen(STARTTIME, ENDTIME, OUTOFMINUTES)
 	offList = off_list_gen(onList, ONMINUTES)
 	setSchedule(onList, offList)
+	#ensure valve is off at exit
+	atexit.register(valve_off)
+	atexit.register(thisPi.stop)
+	atexit.register(conn.close)
+	atexit.register(clean_exit)
 	while 1:
 		time.sleep(1)
 		schedule.run_pending()
